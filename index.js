4533a176505029f72bf002a8cd7f07b8b42a5e0c
alec247 <alec247@gmail.com>
2023-02-09 15:12:31 -0800

    Adds ability to click to add/remove color

--- index.js
+++ index.js
@@ -1,33 +1,46 @@
 const container = document.querySelector("#container");
 
 //creates grid with x amount of rows or cols
-function makeGrid(rows, cols) {
-    if (rows > 100) return;
+function makeGrid(size) {
+    if (((size > 100) || (size <= 1))) return;
   
-    container.style.setProperty("--grid-rows", rows);
-    container.style.setProperty("--grid-cols", cols);
+    container.style.setProperty("--grid-rows", size);
+    container.style.setProperty("--grid-cols", size);
 
-    for (let i = 0; i < (rows * cols); i++) {
+    // for (let i = 0; i < (size ** 2); i++) {
+    //     let cell = document.createElement("div");
+    //     cell.innerText = (i + 1);
+    //     container.appendChild(cell).className = "cell";
+    // }
+
+    for (let i = 0; i < (size); i++) {
         let cell = document.createElement("div");
-        // cell.innerText = (i + 1);
+        cell.innerText = (i + 1);
         container.appendChild(cell).className = "cell";
     }
 }
 
-makeGrid(16, 16);
+if (document.querySelector("#sizeInput").value === "") {
+    makeGrid(16);
+}
+
 
 //Colors cell when moused over
 function colorCell() {
-    this.style.backgroundColor = "red";
+    this.classList.add("colored");
+}
+
+function resetCell() {
+    this.classList.toggle("colored");
 }
 
 const cells = document.querySelectorAll(".cell");
 
 cells.forEach(cell => {
     cell.addEventListener("mouseover", colorCell);
+    cell.addEventListener("click", resetCell);
 })
 
 
 //TODO: 
 //1. Connect input/button to JS
-//3. Add eraser on click
