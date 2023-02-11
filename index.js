423c6f8299b62c5995dd9a1c2bc16b058f91411c
alec247 <alec247@gmail.com>
2023-02-10 17:19:47 -0800

    Fix issue clearing old grid before creating new

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const container = document.querySelector("#container");
 
 //creates grid with x amount of rows or cols
+
 function makeGrid(size) {
     if (((size > 100) || (size <= 1))) return;
-  
+
+    let newCellArray = [];
+
+    // maybe store size in variable and loop through container children X times to removeChildren 1 by 1?    
+
     container.style.setProperty("--grid-rows", size);
     container.style.setProperty("--grid-cols", size);
 
-    // for (let i = 0; i < (size ** 2); i++) {
-    //     let cell = document.createElement("div");
-    //     cell.innerText = (i + 1);
-    //     container.appendChild(cell).className = "cell";
-    // }
-
-    for (let i = 0; i < (size); i++) {
+    for (let i = 0; i < (size ** 2); i++) {
         let cell = document.createElement("div");
         cell.innerText = (i + 1);
-        container.appendChild(cell).className = "cell";
+        cell.className = "cell";
+        newCellArray.push(cell);
     }
+
+    container.replaceChildren(...newCellArray);
+
+    addCellFunctionality();
 }
 
 if (document.querySelector("#sizeInput").value === "") {
@@ -34,13 +38,16 @@ function resetCell() {
     this.classList.toggle("colored");
 }
 
-const cells = document.querySelectorAll(".cell");
+function addCellFunctionality() {
+    let cells = document.querySelectorAll(".cell");
 
-cells.forEach(cell => {
-    cell.addEventListener("mouseover", colorCell);
-    cell.addEventListener("click", resetCell);
-})
+    cells.forEach(cell => {
+        cell.addEventListener("mouseover", colorCell);
+        cell.addEventListener("click", resetCell);
+    })
+}
+
 
 
 //TODO: 
-//1. Connect input/button to JS
+//2. Make squares responsive to fill up space instead of having gap (lower prio)
